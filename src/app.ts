946174d16d58e9bd79268de8259eb81ca4229861
rexim <reximkut@gmail.com>
2018-06-21 02:03:42 +0700

    (#1) Implement Countdown.appendTo

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import * as $ from 'jquery';
+import * as moment from 'moment';
 import Event from './Event';
 import Root from './Root';
 
 new Event(
-    "20-06-2018",
+    moment("22-06-2018", "DD-MM-YYYY").utc(),
     "Tsoding Stream",
     "Streaming some coding on https://twitch.tv/tsoding"
 ).appendTo($('#root'))
 
 new Event(
-    "21-06-2018",
+    moment("23-06-2018", "DD-MM-YYYY").utc(),
     "Tsoding Stream #2",
     "Streaming some coding on https://twitch.tv/tsoding"
 ).appendTo($('#root'))
