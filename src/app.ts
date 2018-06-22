4961af3655d0b4ca71abcdc6061819e65026386a
rexim <reximkut@gmail.com>
2018-06-22 20:20:57 +0700

    Format

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,17 @@ import Event from './Event';
 import Root from './Root';
 
 $('#root')
-    .append(new Event(
-        moment.tz("2018-06-22 18:00", "Asia/Novosibirsk").utc(),
-        "Tsoding Stream",
-        "Streaming some coding on https://twitch.tv/tsoding"
-    ).asHTMLElement())
-    .append(new Event(
-        moment.tz("2018-06-23 18:00", "Asia/Novosibirsk").utc(),
-        "Tsoding Stream #2",
-        "Streaming some coding on https://twitch.tv/tsoding"
-    ).asHTMLElement());
+    .append(
+        new Event(
+            moment.tz("2018-06-22 18:00", "Asia/Novosibirsk").utc(),
+            "Tsoding Stream",
+            "Streaming some coding on https://twitch.tv/tsoding"
+        ).asHTMLElement()
+    )
+    .append(
+        new Event(
+            moment.tz("2018-06-23 18:00", "Asia/Novosibirsk").utc(),
+            "Tsoding Stream #2",
+            "Streaming some coding on https://twitch.tv/tsoding"
+        ).asHTMLElement()
+    );
