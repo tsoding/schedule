230994085ba7bc459c81cce0a872b3e316fe1420
rexim <reximkut@gmail.com>
2018-06-22 17:31:48 +0700

    appendTo -> asHTMLElement

--- src/Countdown.ts
+++ src/Countdown.ts
@@ -7,9 +7,9 @@ export default class Countdown implements UiComponent {
     constructor(private _datetime: moment.Moment) {
     }
 
-    appendTo(entry: JQuery<HTMLElement>): void {
-        entry.append($(`<div title="${this._datetime.tz(moment.tz.guess()).format('llll')}">
-                           ${this._datetime.fromNow()}
-                        </div>`))
+    asHTMLElement(): JQuery<HTMLElement> {
+        return $(`<div title="${this._datetime.tz(moment.tz.guess()).format('llll')}">
+                      ${this._datetime.fromNow()}
+                  </div>`)
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import * as moment from 'moment';
 import Event from './Event';
 import Root from './Root';
 
-new Event(
-    moment.tz("2018-06-22 18:00", "Asia/Novosibirsk").utc(),
-    "Tsoding Stream",
-    "Streaming some coding on https://twitch.tv/tsoding"
-).appendTo($('#root'))
-
-new Event(
-    moment.tz("2018-06-23 18:00", "Asia/Novosibirsk").utc(),
-    "Tsoding Stream #2",
-    "Streaming some coding on https://twitch.tv/tsoding"
-).appendTo($('#root'))
+$('#root')
+    .append(new Event(
+        moment.tz("2018-06-22 18:00", "Asia/Novosibirsk").utc(),
+        "Tsoding Stream",
+        "Streaming some coding on https://twitch.tv/tsoding"
+    ).asHTMLElement())
+    .append(new Event(
+        moment.tz("2018-06-23 18:00", "Asia/Novosibirsk").utc(),
+        "Tsoding Stream #2",
+        "Streaming some coding on https://twitch.tv/tsoding"
+    ).asHTMLElement());
