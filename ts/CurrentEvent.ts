8abc71b1de7f631aab0524586d715e87fdd5a34f
rexim <reximkut@gmail.com>
2018-07-11 16:44:54 +0700

    (#41) Display event ids for convenient copy-paste

--- ts/CurrentEvent.ts
+++ ts/CurrentEvent.ts
@@ -12,6 +12,7 @@ export default class CurrentEvent implements UiComponent {
         let element = $(`<div class="current event">`);
         entry.append(element);
 
+        element.append($(`<div class="timestamp">${this._event.datetime.utc().unix()}</div>`));
         element.append($(`<h1><a href="${this._event.url}">${this._event.title}</a></h1>`));
         new Countdown(this._event.datetime, "started ").appendTo(element);
         element.append($(`<div class="description">${this._event.description}</div>`));
