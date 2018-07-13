2ce808bf9cabfd802b840a20374f91f355b8e719
rexim <reximkut@gmail.com>
2018-07-14 03:03:20 +0700

    (#51) Implement List interface for EventsForDay

--- ts/EventsForDay.ts
+++ ts/EventsForDay.ts
@@ -3,35 +3,44 @@ import * as dto from './dto';
 import * as moment from 'moment';
 import Event from './Event'
 import UiComponent from './UiComponent';
+import List from './List';
 
-export default class EventsForDay implements UiComponent {
+export default class EventsForDay implements UiComponent, List<Event> {
     constructor(private _state: dto.State,
                 private _date: string) {
     }
 
     appendTo(entry: JQuery<HTMLElement>): void {
-        let weekday = moment.tz(this._date, this._state.timezone).isoWeekday()
+        this.asArray().forEach((e) => e.appendTo(entry))
+    }
 
-        this._state.projects.forEach((p) => {
-            if (p.days.includes(weekday)) {
-                new Event({
+    asArray(): Array<Event> {
+        let weekday = moment.tz(this._date, this._state.timezone).isoWeekday();
+        return this._state.projects
+            .filter(
+                (p) => p.days.includes(weekday)
+            )
+            .map(
+                (p) => new Event({
                     datetime: moment.tz(`${this._date} ${p.time}`, this._state.timezone).utc(),
                     title: p.name,
                     description: p.description,
                     url: p.url
-                }, this._state.cancelledEvents).appendTo(entry)
-            }
-        })
-
-        this._state.extraEvents.forEach((ee) => {
-            if (ee.date == this._date) {
-                new Event({
-                    datetime: moment.tz(`${this._date} ${ee.time}`, this._state.timezone).utc(),
-                    title: ee.title,
-                    description: ee.description,
-                    url: ee.url
-                }, this._state.cancelledEvents).appendTo(entry)
-            }
-        })
+                }, this._state.cancelledEvents)
+            )
+            .concat(
+                this._state.extraEvents
+                    .filter(
+                        (ee) => ee.date == this._date
+                    )
+                    .map(
+                        (ee) => new Event({
+                            datetime: moment.tz(`${this._date} ${ee.time}`, this._state.timezone).utc(),
+                            title: ee.title,
+                            description: ee.description,
+                            url: ee.url
+                        }, this._state.cancelledEvents)
+                    )
+            )
     }
 }
