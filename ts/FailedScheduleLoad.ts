70b94b567a401cbb8f8f89961284853992640d18
rexim <reximkut@gmail.com>
2018-07-19 04:00:37 +0700

    TODO(#55)

--- ts/FailedScheduleLoad.ts
+++ ts/FailedScheduleLoad.ts
@@ -3,7 +3,7 @@ import UiComponent from './UiComponent';
 
 export default class FailedScheduleLoad implements UiComponent {
     appendTo(entry: JQuery<HTMLElement>): void {
-        // TODO: FailedScheduleLoad component is not implemented
+        // TODO(#55): FailedScheduleLoad component is not implemented
         entry.append($(`<div>Failed to load schedule data. Keep refreshing.</div>`))
     }
 }
