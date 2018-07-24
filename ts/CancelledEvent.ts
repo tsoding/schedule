2e634e47fb402093ab63d4f69a935d969e621583
rexim <reximkut@gmail.com>
2018-07-24 17:16:24 +0700

    (#72) Fix cancelled event countdown prefix
    Close #72

--- ts/CancelledEvent.ts
+++ ts/CancelledEvent.ts
@@ -15,7 +15,7 @@ export default class CancelledEvent implements UiComponent {
 
         element.append($(`<div class="timestamp"><a href="#_${this._event.datetime.utc().unix()}">${this._event.datetime.utc().unix()}</a></div>`));
         element.append($(`<h1><a href="${this._event.url}">${this._event.title}</a></h1>`));
-        new Countdown(this._event.datetime, "finished ").appendTo(element);
+        new Countdown(this._event.datetime, "should've started ").appendTo(element);
         element.append($(`<div class="description markdown">${this._event.description}</div>`));
         element.append($(`<div class="cancelled-stamp">CANCELLED</div>`));
     }
