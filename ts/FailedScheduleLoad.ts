68961ba040ffbaf6f07041e0da3de7b59a5d7e94
rexim <reximkut@gmail.com>
2018-07-24 17:27:56 +0700

    (#65) Make FailedScheduleLoad use HTML components
    Close #65

--- ts/FailedScheduleLoad.ts
+++ ts/FailedScheduleLoad.ts
@@ -1,10 +1,13 @@
 import * as $ from 'jquery';
 import UiComponent from './UiComponent';
+import Div from './Div'
+import Text from './Text'
 
-// TODO(#65): FailedScheduleLoad doesn't use HTML components
 export default class FailedScheduleLoad implements UiComponent {
     appendTo(entry: JQuery<HTMLElement>): void {
         // TODO(#55): FailedScheduleLoad component is not implemented
-        entry.append($(`<div>Failed to load schedule data. Keep refreshing.</div>`))
+        new Div(
+            new Text('Failed to load schedule data. Keep refreshing.')
+        ).appendTo(entry)
     }
 }
