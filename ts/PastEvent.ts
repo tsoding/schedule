b3e9897507e9d813555ad1af633ba77ebe308432
rexim <reximkut@gmail.com>
2018-08-10 17:55:23 +0700

    (#78) Interpolate ids properly

--- ts/PastEvent.ts
+++ ts/PastEvent.ts
@@ -36,7 +36,7 @@ export default class PastEvent implements UiComponent {
                 )
             ]),
             {"class": "event past",
-             "id": "_${this._event.datetime.utc().unix()}"}
+             "id": `_${this._event.datetime.utc().unix()}`}
         ).appendTo(entry)
     }
 }
