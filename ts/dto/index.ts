a48bfa2718333a4d8d62d3ca585d989e0304edac
rexim <reximkut@gmail.com>
2018-09-04 18:14:09 +0700

    (#83) Introduce PatchedEvent

--- ts/dto/index.ts
+++ ts/dto/index.ts
@@ -1,5 +1,6 @@
 export { default as Event } from './Event';
+export { default as EventPatch } from './EventPatch';
 export { default as ExtraEvent } from './ExtraEvent';
+export { default as PatchedEvent } from './PatchedEvent';
 export { default as Project } from './Project';
 export { default as State } from './State';
-export { default as EventPatch } from './EventPatch';
