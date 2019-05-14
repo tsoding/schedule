f1826a9ec05f210673c95328d4dc3d0f5e39e506
rexim <reximkut@gmail.com>
2019-05-15 04:45:17 +0700

    (#73) Introduce TwitchPlayer into CurrentEvent

---
+++ ts/TwitchPlayer.ts
@@ -0,0 +1,17 @@
+import UiComponent from './UiComponent';
+import * as html from './html';
+
+export default class TwitchPlayer implements UiComponent {
+    constructor(private _userName: string) {
+    }
+
+    appendTo(entry: HTMLElement | null): void {
+        new html.Tag(
+            "iframe",
+            new html.Empty(),
+            {"src": `https://player.twitch.tv/?channel=${this._userName}&muted=true`,
+             "allowfullscreen": "true",
+             "class": "player"}
+        ).appendTo(entry);
+    }
+}
