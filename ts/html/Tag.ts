7aa083e1faf7b7956eb8f2d14546321a74ff1d5c
Nufflee <zifcani@gmail.com>
2019-07-20 01:23:09 +0200

    Add event support to Tag.

--- ts/html/Tag.ts
+++ ts/html/Tag.ts
@@ -3,7 +3,8 @@ import UiComponent from '../UiComponent';
 export default class Tag implements UiComponent {
     constructor(private _name: string,
                 private _body?: UiComponent,
-                private _attrs?: {[key: string]: any}) {
+                private _attrs?: {[key: string]: any},
+                private _events?: {[key: string]: EventListenerOrEventListenerObject}) {
     }
 
     appendTo(entry: HTMLElement | null): void {
@@ -18,6 +19,12 @@ export default class Tag implements UiComponent {
                 let attr = document.createAttribute(attrKey);
                 attr.value = this._attrs[attrKey];
                 element.attributes.setNamedItem(attr);
+            }
+        }
+
+        if (this._events) {
+            for (let eventKey in this._events) {
+                element.addEventListener(eventKey, this._events[eventKey])
             }
         }
 
