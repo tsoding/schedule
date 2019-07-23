1d194118ad95b7ba15632531aeca1bc806442aa2
Arnold Chand <arnold.chand@gmail.com>
2019-07-22 23:03:16 -0400

    remove fontawesome import from app.ts

--- ts/app.ts
+++ ts/app.ts
@@ -6,18 +6,6 @@ import FailedScheduleLoad from './FailedScheduleLoad'
 import StateFromUrl from './StateFromUrl'
 import Weekday from './Weekday'
 
-// FontAwesome imports
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fas)
-library.add(far)
-library.add(fab)
-
-dom.watch()
-
 new StateFromUrl('schedule.json')
     .asPromise()
     .then(
